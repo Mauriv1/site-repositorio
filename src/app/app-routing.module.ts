e04baacb744b01104c99595ce782a6872b358021
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-08-29 01:28:27 -0500

    primer-commit-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", pathMatch:'full',redirectTo:'home'},
+  {path:"nosotros", component:NosotrosComponent},
+  {path:"contacto", component:ContactoComponent},
+  {path:"servicios", component:ServiciosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
