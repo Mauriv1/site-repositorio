412ca69dfc7e166eaebd1063580689079c333139
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-08-29 04:18:15 -0500

    contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
@@ -7,8 +8,8 @@ import { ServiciosComponent } from './components/servicios/servicios.component';
 const routes: Routes = [
   {path:"", pathMatch:'full',redirectTo:'home'},
   {path:"nosotros", component:NosotrosComponent},
-  {path:"contacto", component:ContactoComponent},
-  {path:"servicios", component:ServiciosComponent}
+  {path:"servicios", component:ServiciosComponent},
+  {path:"contacto", component:ContactoComponent}
 ];
 
 @NgModule({
